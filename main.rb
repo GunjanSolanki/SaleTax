29bace5d952bae4d47a350dfe8ad711ac506ec5d
gsolanki <gsolanki@techracers.io>
2019-11-15 11:57:26 +0530

    add main.rb

--- main.rb
+++ main.rb
@@ -8,9 +8,13 @@ puts "Welcome to E-bay!\n"
 puts "Enter number of orders : \n"
 orders = get_input
 
-puts "------------------------------Order-Receipt---------------------------------------"
+puts "\n------------------------------Order-Receipt---------------------------------------"
+
+order_count = 1
 orders.each do |order|
+  puts "Order : #{order_count}"
   output.print_product_details(order)
+  order_count += 1
 end
 
 puts "----------------------------------------------------------------------------------"
